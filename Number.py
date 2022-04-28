032d866615bc09eb5c70e47f02f01d8b50650f62
jangk34 <jangk34@naver.com>
2022-04-28 22:39:44 +0900

    파이썬 숫자형,문자열 자료형

--- Number.py
+++ Number.py
@@ -122,6 +122,161 @@ print(a.split())
 b = "a:b:c:d"
 print(b.split(":"))
 
+#리스트 자료형
+odd = [1,3,5,7,9]
+print(odd)
 
+a = [1,2,3]
+print(a)
+print(a[1])
+print(a[0]+a[2])
 
+a = [1,2,3,['a','b','c']]
+print(a[-1])
+print(a[-1][0])
+print(a[-1][1])
+print(a[-1][2])
+
+a = [1,2, ['a', 'b', ['Life','is']]]
+print(a[2][2][0])
+
+#리스트의 슬라이싱
+a = [1,2,3,4,5]
+print(a[0:2])
+a = "12345"
+print(a[0:2])
+
+a = [1,2,3,4,5]
+b = a[:2]
+c = a[2:]
+print(b,c)
+
+A = [1,2,3,4,5]
+print(A[1:3])
+
+a = [1,2,3, ['a','b','c'],4,5]
+print(a[2:5])
+print(a[3])
+
+#리스트 더하기(+,*)
+a = [1,2,3]
+b = [4,5,6]
+print(a+b)
+print(a*3)
+
+#리스트 길이 구하기
+a = [1,2,3]
+print(len(a))
+
+#리스트 수정
+a = [1,2,3]
+a[2] = 4
+print(a)
+
+#del함수 사용해 리스트 요소 삭제하기
+a = [1,2,3]
+del a[1]
+print(a)
+
+a = [1,2,3,4,5]
+del a[:2]
+print(a)
+
+#append함수 사용해 리스트에 요소 추가
+a = [1,2,3]
+a.append(4)
+print(a)
+a.append([5,6])
+print(a)
+
+#sort함수 리스트 정렬
+a = [1,4,3,2]
+a.sort()
+print(a)
+
+a = ['b','a','c']
+a.sort()
+print(a)
+
+#reverse함수 뒤집기
+a = ['a','c','b']
+a.reverse()
+print(a)
+
+#index함수 배열상 위치 반환
+a = [1,2,3]
+print(a.index(3))
+print(a.index(1))
+
+#insert함수 삽입
+a = [1, 2, 3]
+a.insert(0, 4)
+print(a)
+a.insert(3, 5)
+print(a)
+
+#remove 리스트 요소 제거
+a = [1,2,3,1,2,3]
+a.remove(3) #첫번째 동일 값만 제거, 뒷단의 다른 숫자도 삭제시 한번더 함수 명령
+print(a)
+
+#pop리스트 요소 끄집어내기
+a = [1,2,3]  #리스트의 맨 마지막 요소 돌려주고 그 요소 삭제
+a.pop()
+print(a)
+a = [1,2,3]
+a.pop(1)
+print(a)
+
+#숫자세기
+a = [1,2,3,1]
+print(a.count(1)) #1이라는 숫자
+
+#extend확장
+a = [1,2,3]
+a.extend([4,5])
+print(a)
+b = [6,7]
+a.extend(b)
+print(a)
+
+########## 튜플 자료형
+t = ()
+t2 = (1,) ## 1개값은 뒤에 ,
+t3 = (1,2,3)
+t4 = 1,2,3 ## 가로 없어도 무방
+t5 = ('a','b',('ab', 'cd'))
+
+#튜플 인덱싱
+t1 = (1,2,'a','b')
+print(t1[0])
+print(t1[3])
+print(t1[1:])
+t2 = (3,4)
+print(t1 + t2)
+print(t2 * 3)
+
+#튜플 길이 구하기
+print(len(t1))
+
+t3 = (1,2,3)
+t4 = (4,)
+print(t3 + t4)
+
+#딕셔너리
+dic = {'name':'pey', 'phone':'0119993323', 'birth':'1118'} # key : value
+
+#딕셔너리 쌍 추가, 삭제하기
+a = {1: 'a'}
+a[2] = 'b'
+print(a)
+
+a['name'] = 'pey'
+print(a)
+
+a[3] = [1,2,3]
+print(a)
+
+del a[1] ## 딕셔너리 요소 삭제
+print(a)
 
